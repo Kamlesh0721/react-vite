3ea9cbec3c9fe21dacc3d7ad82773e0b9bc7148a
Kamlesh Singh <kamleshsingh0198@gmail.com>
2024-11-01 23:49:40 +0530

    learn fragments

---
+++ fragment/src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <div>
+      <h1> Unordered List</h1>
+      <ul>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+        <li>Item 5</li>
+      </ul>
+    </div>
+  );
+}
+
+export default App;
