b456d3fc3d8cde0ee8b96eb5db06c2f4c8e3c0db
Kamlesh Singh <kamleshsingh0198@gmail.com>
2024-11-20 21:22:25 +0530

    update react

--- map-method/src/App.jsx
+++ map-method/src/App.jsx
@@ -11,14 +11,42 @@ function App() {
     "Chandni Chowk",
     "Connaught Place",
   ];
+  // let destinations = [];
+
+  // method 1 of conditional rendering
+  // if (destinations.length === 0) {
+  //   return <p>I don't know</p>;
+  // }
+
   return (
+    // Method 2
+    // <>
+
+    //   {destinations.length === 0 ? (
+    //     <p>I don't know</p>
+    //   ) : (
+    //     <>
+    //       <h1> Top Must-Visit Destinations in New Delhi</h1>
+    //       <ul>
+    //         {destinations.map((destination) => (
+    //           <li key={destination}>{destination}</li>
+    //         ))}
+    //       </ul>
+    //     </>
+    //   )}
+    // </>
     <>
-      <h1> Top Must-Visit Destinations in New Delhi</h1>
-      <ul>
-        {destinations.map((destination) => (
-          <li key={destination}>{destination}</li>
-        ))}
-      </ul>
+      {destinations.length === 0 && <p>I don't know</p>}
+      {destinations.length !== 0 && (
+        <>
+          <h1> Top Must-Visit Destinations in New Delhi</h1>
+          <ul>
+            {destinations.map((destination) => (
+              <li key={destination}>{destination}</li>
+            ))}
+          </ul>
+        </>
+      )}
     </>
   );
 }
